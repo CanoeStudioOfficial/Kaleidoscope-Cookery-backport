2d97f30eccf9711556cd441a09d836b90645e665
Lonelyxiya <3438505487@qq.com>
2026-02-21 10:33:55 +0800

    Update ModItems.java

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/content/item/ModItems.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/content/item/ModItems.java
@@ -8,15 +8,5 @@ import java.util.List;
 
 public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<>();
-    
-    public static ScarecrowItem SCARECROW;
-    
-    public static void init() {
-        SCARECROW = register(new ScarecrowItem());
-    }
-    
-    private static <T extends Item> T register(T item) {
-        ITEMS.add(item);
-        return item;
-    }
+
 }
